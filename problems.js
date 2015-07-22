405af2b47ddb6cb2fb83a05a8a920c8905e546f3
Adam Smith <atsmith227@gmail.com>
2015-07-22 15:21:52 -0600

    Initial commit

---
+++ problems.js
@@ -0,0 +1,5 @@
+// 1. Write a function addNumbers that takes a single string parameter and searches for all the numbers in the string, adds them together, then returns the sum. 
+
+// 2. Write a function longestWord that takes a single string parameter and returns the largest word in the string.
+
+// 3. Write a function averageStringNumbers that takes a single string parameter and searches for all the numbers in the string, adds them together, then returns that final number divided by the total amount of letters in the string.
